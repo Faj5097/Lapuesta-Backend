db62393e2095b2c6b63cd1e2f2389a968c38ce6e
Faj5097 <fab.jan@t-online.de>
2021-05-24 00:37:05 +0200

    No odds for zero Games

--- odds.js
+++ odds.js
@@ -4,9 +4,9 @@ function getOdds(newMatchUp, player1, player2) {
   const draw = parseInt(player1.draws + player2.draws);
   const sum = player1Wins + player2Wins + draw;
 
-  const oddPlayer1Wins = (95 / ((player1Wins / sum) * 100)).toFixed(2);
-  const oddPlayer2Wins = (95 / ((player2Wins / sum) * 100)).toFixed(2);
-  const oddDraw = (95 / ((draw / sum) * 100)).toFixed(2);
+  const oddPlayer1Wins = player1Wins === 0 ? 2 : (95 / ((player1Wins / sum) * 100)).toFixed(2);
+  const oddPlayer2Wins = player2Wins === 0 ? 2 : (95 / ((player2Wins / sum) * 100)).toFixed(2);
+  const oddDraw = draw === 0 ? 2 :  (95 / ((draw / sum) * 100)).toFixed(2);
 
   newMatchUp.probability.player1Wins = oddPlayer1Wins;
   newMatchUp.probability.draw = oddDraw;
